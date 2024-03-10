6c76a7e4f9754537a4624e5f6892126bdb9d02df
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-03-10 21:24:55 +0300

    react-icons

---
+++ sanity-nextjs-portfolio-site/schemaTypes/profile.ts
@@ -0,0 +1,14 @@
+// schemaTypes/profile.ts
+
+import { defineField } from "sanity";
+import { BiUser } from "react-icons/bi";
+
+const profile = {
+  name: "profile",
+  title: "Profile",
+  type: "document",
+  icon: BiUser,
+  fields: [],
+};
+
+export default profile;
